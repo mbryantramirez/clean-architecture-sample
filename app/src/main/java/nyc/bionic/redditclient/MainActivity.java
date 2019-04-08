2d1408a87c4b6510aaa3bfcc3489b2c575e508ea
Bryant R <brfarciert@gmail.com>
2019-04-08 17:20:55 -0400

    first commit

---
+++ app/src/main/java/nyc/bionic/redditclient/MainActivity.java
@@ -0,0 +1,13 @@
+package nyc.bionic.redditclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
